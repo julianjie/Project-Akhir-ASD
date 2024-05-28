da1e934946a41d9c9ca03370228e6a82367963d3
julian <julian.jie78@gmail.com>
2024-05-28 21:51:07 +0700

    add class orang & pasien

---
+++ src/Orang.java
@@ -0,0 +1,22 @@
+
+public class Orang {
+	private String nama;
+	private byte umur;
+	
+	public Orang(String nama,byte umur) {
+		this.nama = nama;
+		this.umur = umur;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public byte getUmur() {
+		return umur;
+	}
+	public void setUmur(byte umur) {
+		this.umur = umur;
+	}
+}
